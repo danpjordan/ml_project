98fc361e839a7d11c22c617c70fe32e9cf7a061b
daniel <danpjordan59@yahoo.com>
2024-07-30 23:50:05 -0400

    Added Mapping for String to Int
    Adding a function to map all dataframe string to a integer and store that map in the column object

--- dataframemaker.py
+++ dataframemaker.py
@@ -21,11 +21,27 @@ class Column:
   def stringToNum(self, string):
     return self.string_to_num[string]
   
-  def addStringAndNum(self, string, num):
+  # adds a string-num and num-string conversion to the column
+  def addStringNum(self, string, num):
     self.string_to_num[string] = num
     self.num_to_string[num] = string
     
+  def checkString(self, string):
+    return string in self.string_to_num
+  
+  def printDict(self):
+    print("Dictionary:")
+    for key in self.num_to_string:
+      print(key, self.num_to_string[key])
+    
+  def printColumn(self):
+    print("String:", self.isString())
+    print("Discrete:", self.isDiscrete())
+    if self.isString():
+      self.printDict()
 
+
+# create all the column objects based on data in dataframe
 def createColumnDict(df):
   columns = {}
   for column in df:
@@ -36,32 +52,44 @@ def createColumnDict(df):
 
   return columns
 
+def printColumns(columns):
+  for column in columns:
+    print(column)
+    columns[column].printColumn()
+    print("--")
+
+# converts all strings of a dataframe to integers and stores add the dictionary entry to the column object
+def convertStringToInt(df, columns):
+  # itterate through all column names in columns dictionary
+  for col in columns:
+    # if the column is a string, convery entry to a mapped integer
+    if (columns[col].isString()):
+      current_index = 0
+      for i, row in enumerate(df[col]):
+        # check if string is in map, if not add a new entry
+        if not columns[col].checkString(row):
+          columns[col].addStringNum(row, current_index)
+          current_index = current_index + 1
+        
+        # change the entry to the mapped value
+        df.loc[i, col] = columns[col].stringToNum(row)
 
 def main():
-
   data_name = "data/Employee-Attrition.csv"
   output_name = "output/out.csv"
 
   imported_df = pd.read_csv(data_name)
   
   # creates a map of columns with key = column name,
-  # value is column object containing data about the column
+  # value is a column object containing data about the column
   columns = createColumnDict(imported_df)
   
-  # converts the columns of strings to ints and stores the map in column object
-  for col in columns:
-    # print(col, columns[col].isString(), columns[col].isDiscrete())
-    if (columns[col].isString()):
-      print(col)
-      
-  imported_df.to_csv(output_name, index=False)
-  
-  c = Column(True, True)
-  c.addStringAndNum("Yes", 1)
-  print(c.stringToNum("Yes"))
-  print(c.numToString(1))
-  
+  # converts the columns of strings to integers and stores the map in column object
+  convertStringToInt(imported_df, columns)
 
+  printColumns(columns)
+  
+  imported_df.to_csv(output_name, index=False)
     
 if __name__ == '__main__':
   main()
